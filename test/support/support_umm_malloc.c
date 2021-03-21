8fae4e713371b410b1d7cee032d0bc0b37b90939
Ralph Hempel <rhempel@hempeldesigngroup.com>
2021-03-21 17:21:33 +0100

    [UMM-35] Clean up the test code with uncrustify

--- test/support/support_umm_malloc.c
+++ test/support/support_umm_malloc.c
@@ -14,18 +14,18 @@
 uint32_t UMM_MALLOC_CFG_HEAP_SIZE = SUPPORT_UMM_MALLOC_HEAP_SIZE;
 
 char test_umm_heap[0x2000][UMM_BLOCK_BODY_SIZE];
-void * UMM_MALLOC_CFG_HEAP_ADDR = &test_umm_heap;
+void *UMM_MALLOC_CFG_HEAP_ADDR = &test_umm_heap;
 
 int umm_max_critical_depth;
 int umm_critical_depth;
 
-bool check_all_bytes ( uint8_t * p, size_t s, uint8_t v) {
-	while ( (*p == v) && s ) {
-		++p;
-	 	--s;
-        }
+bool check_all_bytes(uint8_t *p, size_t s, uint8_t v) {
+    while ((*p == v) && s) {
+        ++p;
+        --s;
+    }
 
-	return ( s == 0 );
+    return s == 0;
 }
 
 // Note, the get_xxx() functions depend on knowledge of the internals
@@ -39,58 +39,56 @@ bool check_all_bytes ( uint8_t * p, size_t s, uint8_t v) {
 // 2 - body.free.next
 // 3 - body.free.prev
 
-bool get_block_is_free ( int  b ) {
-    return ( (((uint16_t *)(&test_umm_heap[b]))[0] & 0x8000) == 0x8000 );
+bool get_block_is_free(int b) {
+    return (((uint16_t *)(&test_umm_heap[b]))[0] & 0x8000) == 0x8000;
 }
 
-uint16_t get_block_next ( int b ) {
-    return ( ((uint16_t *)(&test_umm_heap[b]))[0] & 0x7FFF );
+uint16_t get_block_next(int b) {
+    return ((uint16_t *)(&test_umm_heap[b]))[0] & 0x7FFF;
 }
 
-uint16_t get_block_prev ( int b ) {
-    return ( (((uint16_t *)(&test_umm_heap[b]))[1]));
+uint16_t get_block_prev(int b) {
+    return ((uint16_t *)(&test_umm_heap[b]))[1];
 }
 
-uint16_t get_block_next_free ( int b ) {
-    return ( (((uint16_t *)(&test_umm_heap[b]))[2]));
+uint16_t get_block_next_free(int b) {
+    return ((uint16_t *)(&test_umm_heap[b]))[2];
 }
 
-uint16_t get_block_prev_free ( int b ) {
-    return ( (((uint16_t *)(&test_umm_heap[b]))[3]));
+uint16_t get_block_prev_free(int b) {
+    return ((uint16_t *)(&test_umm_heap[b]))[3];
 }
 
 char block_test_msg[TEST_MSG_LEN];
 char block_actual_msg[TEST_MSG_LEN];
 char test_msg[256];
 
-bool check_block (struct block_test_values *t)
-{
-        snprintf (block_test_msg,   TEST_MSG_LEN, "\nTest__: Block %04d f %d n %04d p %04d nf %04d pf %04d", t->block
-                                                                                                           , t->is_free
-                                                                                                           , t->next
-                                                                                                           , t->prev
-                                                                                                           , t->next_free
-                                                                                                           , t->prev_free);
-        snprintf (block_actual_msg, TEST_MSG_LEN, "\nActual: Block %04d f %d n %04d p %04d nf %04d pf %04d\n", t->block
-                                                                                                           , get_block_is_free (t->block)
-                                                                                                           , get_block_next(t->block)
-                                                                                                           , get_block_prev(t->block)
-                                                                                                           , get_block_next_free(t->block)
-                                                                                                           , get_block_prev_free(t->block));
-        strncpy( test_msg, block_test_msg, 256 );
-        strncat( test_msg, block_actual_msg, 256 );
+bool check_block(struct block_test_values *t) {
+    snprintf(block_test_msg,   TEST_MSG_LEN, "\nTest__: Block %04d f %d n %04d p %04d nf %04d pf %04d", t->block
+        , t->is_free
+        , t->next
+        , t->prev
+        , t->next_free
+        , t->prev_free);
+    snprintf(block_actual_msg, TEST_MSG_LEN, "\nActual: Block %04d f %d n %04d p %04d nf %04d pf %04d\n", t->block
+        , get_block_is_free(t->block)
+        , get_block_next(t->block)
+        , get_block_prev(t->block)
+        , get_block_next_free(t->block)
+        , get_block_prev_free(t->block));
+    strncpy(test_msg, block_test_msg, 256);
+    strncat(test_msg, block_actual_msg, 256);
 
-        TEST_ASSERT_EQUAL_MESSAGE ( t->is_free,   get_block_is_free(   t->block ), test_msg );
-        TEST_ASSERT_EQUAL_MESSAGE ( t->next,      get_block_next(      t->block ), test_msg );
-        TEST_ASSERT_EQUAL_MESSAGE ( t->prev,      get_block_prev(      t->block ), test_msg );
-        TEST_ASSERT_EQUAL_MESSAGE ( t->next_free, get_block_next_free( t->block ), test_msg );
-        TEST_ASSERT_EQUAL_MESSAGE ( t->prev_free, get_block_prev_free( t->block ), test_msg );
+    TEST_ASSERT_EQUAL_MESSAGE(t->is_free,   get_block_is_free(t->block), test_msg);
+    TEST_ASSERT_EQUAL_MESSAGE(t->next,      get_block_next(t->block), test_msg);
+    TEST_ASSERT_EQUAL_MESSAGE(t->prev,      get_block_prev(t->block), test_msg);
+    TEST_ASSERT_EQUAL_MESSAGE(t->next_free, get_block_next_free(t->block), test_msg);
+    TEST_ASSERT_EQUAL_MESSAGE(t->prev_free, get_block_prev_free(t->block), test_msg);
 
-        return true;
+    return true;
 }
 
-size_t normalize_allocation_size(size_t s)
-{
+size_t normalize_allocation_size(size_t s) {
     size_t first_block;
     size_t full_blocks;
     size_t extra_bytes;
@@ -103,187 +101,179 @@ size_t normalize_allocation_size(size_t s)
     // 2. Number of additional full blocks
     // 3. Any extra bytes
 
-    if (s <= (UMM_MIN_BLOCK_BODY_SIZE-UMM_BLOCK_HEADER_SIZE)) {
+    if (s <= (UMM_MIN_BLOCK_BODY_SIZE - UMM_BLOCK_HEADER_SIZE)) {
         first_block = 0;
         full_blocks = 0;
         extra_bytes = s;
     } else {
         first_block = 1;
-        full_blocks = ((s-(UMM_MIN_BLOCK_BODY_SIZE-UMM_BLOCK_HEADER_SIZE))/UMM_MIN_BLOCK_BODY_SIZE);
-        extra_bytes= (s-(UMM_MIN_BLOCK_BODY_SIZE-UMM_BLOCK_HEADER_SIZE)-(full_blocks*UMM_MIN_BLOCK_BODY_SIZE));
+        full_blocks = ((s - (UMM_MIN_BLOCK_BODY_SIZE - UMM_BLOCK_HEADER_SIZE)) / UMM_MIN_BLOCK_BODY_SIZE);
+        extra_bytes = (s - (UMM_MIN_BLOCK_BODY_SIZE - UMM_BLOCK_HEADER_SIZE) - (full_blocks * UMM_MIN_BLOCK_BODY_SIZE));
     }
 
-    return (first_block*UMM_FIRST_BLOCK_BODY_SIZE + full_blocks*UMM_BLOCK_BODY_SIZE + extra_bytes);
+    return first_block * UMM_FIRST_BLOCK_BODY_SIZE + full_blocks * UMM_BLOCK_BODY_SIZE + extra_bytes;
 }
 
-bool check_blocks (struct block_test_values *t, size_t n)
-{
+bool check_blocks(struct block_test_values *t, size_t n) {
     int i;
-    for (i=0; i<n; ++i) {
-        TEST_ASSERT_TRUE (check_block (&t[i]));
+    for (i = 0; i < n; ++i) {
+        TEST_ASSERT_TRUE(check_block(&t[i]));
     }
-    return (true);
+    return true;
 }
 
 static uint64_t seed = 0;
 
-void srand32(uint32_t s)
-{
+void srand32(uint32_t s) {
     seed = s;
 }
 
-uint32_t rand32(void)
-{
+uint32_t rand32(void) {
     seed = (uint32_t)(((uint64_t)2862933555777941757 * seed) + 3037000493);
 
-    return seed>>10;
+    return seed >> 10;
 }
 
 #define STRESS_TEST_ENTRIES (256)
 
 #ifdef UMM_POISON_CHECK
 struct umm_test_functions umm_test_poison = {
-   umm_poison_malloc,
-   umm_poison_calloc,
-   umm_poison_realloc,
-   umm_poison_free,
-   umm_poison_check,
+    umm_poison_malloc,
+    umm_poison_calloc,
+    umm_poison_realloc,
+    umm_poison_free,
+    umm_poison_check,
 };
 #else
-bool  umm_check( void ) {
+bool  umm_check(void) {
     return true;
 }
 
 struct umm_test_functions umm_test_poison = {
-   umm_malloc,
-   umm_calloc,
-   umm_realloc,
-   umm_free,
-   umm_check,
+    umm_malloc,
+    umm_calloc,
+    umm_realloc,
+    umm_free,
+    umm_check,
 };
 #endif
 
 #define UMM_TEST_GETTIME(a) (clock_gettime(CLOCK_REALTIME, &a))
 
-#define UMM_TEST_DIFFTIME(a,b) ((b.tv_sec - a.tv_sec) * (uint64_t)(1000*1000*1000) \
-                                 + (b.tv_nsec - a.tv_nsec +100))
+#define UMM_TEST_DIFFTIME(a,b) ((b.tv_sec - a.tv_sec) * (uint64_t)(1000 * 1000 * 1000) \
+    + (b.tv_nsec - a.tv_nsec + 100))
 
-uint64_t stress_test( int iterations, struct umm_test_functions *f )
-{
-  void *p[STRESS_TEST_ENTRIES];
-  int i,j,k;
-  size_t s;
+uint64_t stress_test(int iterations, struct umm_test_functions *f) {
+    void *p[STRESS_TEST_ENTRIES];
+    int i,j,k;
+    size_t s;
 
-  uint64_t umm_malloc_time = 0;
+    uint64_t umm_malloc_time = 0;
 
-  struct timespec start, end;
+    struct timespec start, end;
 
-  srand32(0);
+    srand32(0);
 
-  for( j=0; j<STRESS_TEST_ENTRIES; ++j )
-    p[j] = (void *)NULL;
-
-  for( i=0; i<iterations; ++i ) {
-    j = rand32()%STRESS_TEST_ENTRIES;
-
-    switch( rand32() % 16 ) {
-
-      case  0:
-      case  1:
-      case  2:
-      case  3:
-      case  4:
-      case  5:
-      case  6:
-        {
-          UMM_TEST_GETTIME(start);
-          p[j] = f->umm_test_realloc(p[j], 0);
-          UMM_TEST_GETTIME(end);
-          umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
-
-          TEST_ASSERT_NULL( p[j] );
-          break;
-        }
-      case  7:
-      case  8:
-        {
-          s = normalize_allocation_size(rand32()%64);
-          UMM_TEST_GETTIME(start);
-          p[j] = f->umm_test_realloc(p[j], s );
-          UMM_TEST_GETTIME(end);
-          umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
-
-          if( s ) {
-              TEST_ASSERT_NOT_NULL( p[j] );
-              memset(p[j], 0xfe, s);
-          } else {
-              TEST_ASSERT_NULL( p[j] );
-          }
-          break;
-        }
-
-      case  9:
-      case 10:
-      case 11:
-      case 12:
-        {
-          s = normalize_allocation_size(rand32()%100);
-          UMM_TEST_GETTIME(start);
-          p[j] = f->umm_test_realloc(p[j], s);
-          UMM_TEST_GETTIME(end);
-          umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
-
-          if( s ) {
-              TEST_ASSERT_NOT_NULL( p[j] );
-              memset(p[j], 0xfe, s);
-          } else {
-              TEST_ASSERT_NULL( p[j] );
-          }
-          break;
-        }
-
-      case 13:
-      case 14:
-        {
-          s = normalize_allocation_size(rand32()%200);
-          UMM_TEST_GETTIME(start);
-          f->umm_test_free(p[j]);
-          p[j] = f->umm_test_calloc( 1, s );
-          UMM_TEST_GETTIME(end);
-          umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
-
-          if( s ) {
-              TEST_ASSERT_NOT_NULL( p[j] );
-              TEST_ASSERT_TRUE( check_all_bytes(p[j], s, 0x00) );
-              memset(p[j], 0xfe, s);
-          } else {
-              TEST_ASSERT_NULL( p[j] );
-          }
-          break;
-        }
-
-      default:
-        {
-          s = normalize_allocation_size(rand32()%400);
-          UMM_TEST_GETTIME(start);
-          f->umm_test_free(p[j]);
-          p[j] = f->umm_test_malloc( s );
-          UMM_TEST_GETTIME(end);
-          umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
-
-          if( s ) {
-              TEST_ASSERT_NOT_NULL( p[j] );
-              memset(p[j], 0xfe, s);
-          } else {
-              TEST_ASSERT_NULL( p[j] );
-          }
-          break;
-        }
+    for (j = 0; j < STRESS_TEST_ENTRIES; ++j) {
+        p[j] = (void *)NULL;
     }
 
-    TEST_ASSERT_NOT_EQUAL(0, INTEGRITY_CHECK());
-    TEST_ASSERT_NOT_EQUAL(0, POISON_CHECK());
-  }
+    for (i = 0; i < iterations; ++i) {
+        j = rand32() % STRESS_TEST_ENTRIES;
 
-  return umm_malloc_time;
+        switch (rand32() % 16) {
+
+            case  0:
+            case  1:
+            case  2:
+            case  3:
+            case  4:
+            case  5:
+            case  6: {
+                UMM_TEST_GETTIME(start);
+                p[j] = f->umm_test_realloc(p[j], 0);
+                UMM_TEST_GETTIME(end);
+                umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
+
+                TEST_ASSERT_NULL(p[j]);
+                break;
+            }
+            case  7:
+            case  8: {
+                s = normalize_allocation_size(rand32() % 64);
+                UMM_TEST_GETTIME(start);
+                p[j] = f->umm_test_realloc(p[j], s);
+                UMM_TEST_GETTIME(end);
+                umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
+
+                if (s) {
+                    TEST_ASSERT_NOT_NULL(p[j]);
+                    memset(p[j], 0xfe, s);
+                } else {
+                    TEST_ASSERT_NULL(p[j]);
+                }
+                break;
+            }
+
+            case  9:
+            case 10:
+            case 11:
+            case 12: {
+                s = normalize_allocation_size(rand32() % 100);
+                UMM_TEST_GETTIME(start);
+                p[j] = f->umm_test_realloc(p[j], s);
+                UMM_TEST_GETTIME(end);
+                umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
+
+                if (s) {
+                    TEST_ASSERT_NOT_NULL(p[j]);
+                    memset(p[j], 0xfe, s);
+                } else {
+                    TEST_ASSERT_NULL(p[j]);
+                }
+                break;
+            }
+
+            case 13:
+            case 14: {
+                s = normalize_allocation_size(rand32() % 200);
+                UMM_TEST_GETTIME(start);
+                f->umm_test_free(p[j]);
+                p[j] = f->umm_test_calloc(1, s);
+                UMM_TEST_GETTIME(end);
+                umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
+
+                if (s) {
+                    TEST_ASSERT_NOT_NULL(p[j]);
+                    TEST_ASSERT_TRUE(check_all_bytes(p[j], s, 0x00));
+                    memset(p[j], 0xfe, s);
+                } else {
+                    TEST_ASSERT_NULL(p[j]);
+                }
+                break;
+            }
+
+            default: {
+                s = normalize_allocation_size(rand32() % 400);
+                UMM_TEST_GETTIME(start);
+                f->umm_test_free(p[j]);
+                p[j] = f->umm_test_malloc(s);
+                UMM_TEST_GETTIME(end);
+                umm_malloc_time += UMM_TEST_DIFFTIME(start,end);
+
+                if (s) {
+                    TEST_ASSERT_NOT_NULL(p[j]);
+                    memset(p[j], 0xfe, s);
+                } else {
+                    TEST_ASSERT_NULL(p[j]);
+                }
+                break;
+            }
+        }
+
+        TEST_ASSERT_NOT_EQUAL(0, INTEGRITY_CHECK());
+        TEST_ASSERT_NOT_EQUAL(0, POISON_CHECK());
+    }
+
+    return umm_malloc_time;
 }
--- test/test_Metrics.c
+++ test/test_Metrics.c
@@ -15,181 +15,183 @@
     #include "dbglog/dbglog.h"
 #endif
 
-void setUp(void)
-{
+void setUp(void) {
     umm_init();
     umm_critical_depth = 0;
     umm_max_critical_depth = 0;
 }
 
-void tearDown(void)
-{
-    TEST_ASSERT_LESS_OR_EQUAL (1, umm_max_critical_depth);
+void tearDown(void) {
+    TEST_ASSERT_LESS_OR_EQUAL(1, umm_max_critical_depth);
 }
 
-void testMetricsRandom(void)
-{
+void testMetricsRandom(void) {
     int p[1000];
     int i;
- 
-    for (i=0; i<1000; ++i) {
+
+    for (i = 0; i < 1000; ++i) {
         p[i] = 0;
     }
 
     srand32(0);
 
-    for (i=0; i<(1000*1000*1000); ++i) {
-       p[rand32()%1000]++;
+    for (i = 0; i < (1000 * 1000 * 1000); ++i) {
+        p[rand32() % 1000]++;
     }
 
-    for (i=0; i<1000; ++i) {
-        TEST_ASSERT_LESS_OR_EQUAL(1*1000*1000 + 3600, p[i]);
-        TEST_ASSERT_GREATER_OR_EQUAL(1*1000*1000 - 3600, p[i]);
+    for (i = 0; i < 1000; ++i) {
+        TEST_ASSERT_LESS_OR_EQUAL(1 * 1000 * 1000 + 3600, p[i]);
+        TEST_ASSERT_GREATER_OR_EQUAL(1 * 1000 * 1000 - 3600, p[i]);
     }
 }
 
-void testMetricsEmpty(void)
-{
-#ifdef UMM_INLINE_METRICS
-    TEST_ASSERT_EQUAL (0, umm_fragmentation_metric());
-#endif
+void testMetricsEmpty(void) {
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(0, umm_fragmentation_metric());
+    #endif
     umm_info(0, false);
-    TEST_ASSERT_EQUAL (0, umm_fragmentation_metric());
+    TEST_ASSERT_EQUAL(0, umm_fragmentation_metric());
 }
 
-void testMetricsFull(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsFull(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<(UMM_LASTBLOCK-1); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < (UMM_LASTBLOCK - 1); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (0, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (0, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(0, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(0, umm_fragmentation_metric());
 }
 
-void testMetricsSparseFull(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparseFull(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<(UMM_LASTBLOCK); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < (UMM_LASTBLOCK); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<(UMM_LASTBLOCK); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < (UMM_LASTBLOCK); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (99, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (99, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(99, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(99, umm_fragmentation_metric());
 }
 
-void testMetricsSparse7of8(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparse7of8(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<((UMM_LASTBLOCK*7)/8); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < ((UMM_LASTBLOCK * 7) / 8); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<((UMM_LASTBLOCK*7)/8); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < ((UMM_LASTBLOCK * 7) / 8); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (78, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (78, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(78, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(78, umm_fragmentation_metric());
 }
 
-void testMetricsSparse3of4(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparse3of4(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<((UMM_LASTBLOCK*3)/4); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < ((UMM_LASTBLOCK * 3) / 4); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<((UMM_LASTBLOCK*3)/4); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < ((UMM_LASTBLOCK * 3) / 4); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (61, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (61, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(61, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(61, umm_fragmentation_metric());
 }
 
-void testMetricsSparse1of2(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparse1of2(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<((UMM_LASTBLOCK*1)/2); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < ((UMM_LASTBLOCK * 1) / 2); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<((UMM_LASTBLOCK*1)/2); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < ((UMM_LASTBLOCK * 1) / 2); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (34, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (34, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(34, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(34, umm_fragmentation_metric());
 }
 
-void testMetricsSparse1of4(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparse1of4(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<((UMM_LASTBLOCK*1)/4); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < ((UMM_LASTBLOCK * 1) / 4); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<((UMM_LASTBLOCK*1)/4); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < ((UMM_LASTBLOCK * 1) / 4); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (15, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (15, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(15, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(15, umm_fragmentation_metric());
 }
 
-void testMetricsSparse1of8(void)
-{
-  void *p[UMM_LASTBLOCK];
-  int i;
+void testMetricsSparse1of8(void) {
+    void *p[UMM_LASTBLOCK];
+    int i;
 
-  memset(p, sizeof(p), 0);
+    memset(p, sizeof(p), 0);
 
-  for( i=0; i<((UMM_LASTBLOCK*1)/8); ++i )
-    p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    for (i = 0; i < ((UMM_LASTBLOCK * 1) / 8); ++i) {
+        p[i] = umm_malloc(UMM_FIRST_BLOCK_BODY_SIZE);
+    }
 
-  for( i=1; i<((UMM_LASTBLOCK*1)/8); i+=2 )
-    umm_free(p[i]);
+    for (i = 1; i < ((UMM_LASTBLOCK * 1) / 8); i += 2) {
+        umm_free(p[i]);
+    }
 
-#ifdef UMM_INLINE_METRICS
-  TEST_ASSERT_EQUAL (7, umm_fragmentation_metric());
-#endif
-  umm_info(0, false);
-  TEST_ASSERT_EQUAL (7, umm_fragmentation_metric());
+    #ifdef UMM_INLINE_METRICS
+    TEST_ASSERT_EQUAL(7, umm_fragmentation_metric());
+    #endif
+    umm_info(0, false);
+    TEST_ASSERT_EQUAL(7, umm_fragmentation_metric());
 }
