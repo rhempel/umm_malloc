f99d71d5f42a84fba0397e6d58cdd378457303b4
Katherine Whitlock <kate@skylinesynths.nyc>
2023-07-06 10:08:05 -0400

    Add support for multi-heaps

--- src/umm_malloc.h
+++ src/umm_malloc.h
@@ -9,10 +9,27 @@
 #define UMM_MALLOC_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+/* ------------------------------------------------------------------------ */
+
+typedef struct umm_heap_config {
+    void *pheap;
+    size_t heap_size;
+    uint16_t numblocks;
+} umm_heap;
+
+extern void  umm_multi_init_heap(umm_heap *heap, void *ptr, size_t size);
+extern void  umm_multi_init(umm_heap *heap);
+
+extern void *umm_multi_malloc(umm_heap *heap, size_t size);
+extern void *umm_multi_calloc(umm_heap *heap, size_t num, size_t size);
+extern void *umm_multi_realloc(umm_heap *heap, void *ptr, size_t size);
+extern void  umm_multi_free(umm_heap *heap, void *ptr);
 
 /* ------------------------------------------------------------------------ */
 
